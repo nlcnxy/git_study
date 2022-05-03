f606345fe8d71f1c7e1e04af9a18e833bca3dfab
nlcnxy <1136158084@qq.com>
2022-05-03 14:49:24 +0800

    push commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println("吼吼吼4444");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
